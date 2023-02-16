1e7e35f988e53fc1eede089ffe73a7194e2b6500
Ju <97508841+haruharuganda@gmail.com>
2023-02-16 14:48:40 +0900

    tailwind 오류해결

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    fontSize: {
-      sm: ["16px", "19px"],
-    },
-    colors: {
-      sora: "#538EDF",
-      nam: "#015397",
-      parang: "#1098ED",
-      textNavy: "#12396F",
+    extend: {
+      fontSize: {
+        sm: ["16px", "19px"],
+      },
+      colors: {
+        sora: "#538EDF",
+        nam: "#015397",
+        parang: "#1098ED",
+        textNavy: "#12396F",
+      },
     },
   },
   plugins: [],
