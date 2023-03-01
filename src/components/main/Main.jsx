4b041889be4e2b278ab66fae89797073e790a289
Ju <97508841+haruharuganda@gmail.com>
2023-03-02 01:24:40 +0900

    Feat/Router  #92 Router정리

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
-import defaultprofileImg from "../../img/User-86.png";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import InfiniteScroll from "./InfiniteScroll";
 import { useDispatch, useSelector } from "react-redux";
 import MainScheduleCards from "./MainScheduleCards";
-import ScheduleSlice, {
-  __getSchedule,
-} from "../../redux/modules/ScheduleSlice";
+import { __getSchedule } from "../../redux/modules/ScheduleSlice";
 import BottomNavi from "../layout/BottomNavi";
 import desc from "../../img/desc.png";
 import SelectorSort from "../modal/SelectorSort";
@@ -15,12 +11,11 @@ import SelectorSort from "../modal/SelectorSort";
 const Main = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const nickName = useState(sessionStorage.getItem("nickname"));
+  const getRandom = Math.floor(Math.random() * (4 + 0) + 0);
   const { schedules } = useSelector((state) => state.ScheduleSlice);
-  const [profileImg, setProfileImg] = useState(defaultprofileImg);
-  const [nickName, setNickname] = useState(sessionStorage.getItem("nickname"));
   const [sortList, setSortList] = useState("D-Day");
   const [modalOpen, setModalOpen] = useState(false);
-  const getRandom = Math.floor(Math.random() * (4 + 0) + 0);
   const welcomText = [
     "환영합니다.",
     "오늘도 그님스가 응원할게요.",
@@ -33,44 +28,26 @@ const Main = () => {
   };
 
   useEffect(() => {
-    const getNickname = sessionStorage.getItem("nickname");
-    const getprofilImg = sessionStorage.getItem("profileImage");
     const userId = sessionStorage.getItem("userId");
-
-    if (getNickname && userId) {
-      if (getprofilImg) {
-        setProfileImg(getprofilImg);
-      }
-      dispatch(__getSchedule(userId));
-    } else {
-      navigate(`/login`);
-    }
-  }, [navigate, nickName, dispatch]);
+    dispatch(__getSchedule(userId));
+  }, [dispatch]);
 
   return (
     <>
       <div className="container">
         <div className="grid grid-flow-row gap-[26px] ml-[20px] mr-[20px] mb-[75px]">
           <div className="mt-[30px] h-[56px] bg-[#FFFFFF] rounded-[10px] drop-shadow-lg">
-            <div className>
-              <div className>
                 <p className="leading-[36px] p-[10px] font-[700] text-[20px] text-textNavy">
                   {nickName}님 {welcomText[getRandom]}
                 </p>
-              </div>
-            </div>
           </div>
           <div className="flex cursor-pointer ">
-            <div
+            <div 
               onClick={showModalHandler}
-              className=" bg-white  flex gap-[6px] p-[10px] text-center items-center h-[40px] border border-solid border-[#E8E8E8] rounded-[39px] shadow-md"
+              className="bg-white  flex gap-[6px] p-[10px] text-center items-center h-[40px] border border-solid border-[#E8E8E8] rounded-[39px] shadow-md"
             >
-              <div className="shadow-sm  ">
-                <img
-                  src={desc}
-                  className="h-[15.5px] w-[13.54px]"
-                  alt="화살표모양, 오름차순정렬"
-                />
+              <div className="shadow-sm">
+                <img src={desc} className="h-[15.5px] w-[13.54px]" alt="화살표모양, 오름차순정렬"/>
               </div>
               <div>{sortList} 순</div>
             </div>
@@ -89,9 +66,7 @@ const Main = () => {
       </div>
       <div className="fixed bottom-0">
         {modalOpen ? false : <BottomNavi />}
-        {modalOpen && (
-          <SelectorSort setModalOpen={setModalOpen} setSortList={setSortList} />
-        )}
+        {modalOpen && (<SelectorSort setModalOpen={setModalOpen} setSortList={setSortList} /> )}
       </div>
     </>
   );
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate} from "react-router-dom";
 import Layout from "../components/layout/Layout";
-import styled from "styled-components";
 import LoginPage from "../page/LoginPage";
 import SignupPage from "../page/SingupPage";
 import KakaoLogin from "../components/login/KakaoLogin";
@@ -18,7 +17,6 @@ import SetProfileImgPage from "../page/SetProfileImgPage";
 import NotificationsPage from "../page/NotificationsPage";
 import ScheduleInvitation from "../components/mypage/ScheduleInvitation";
 import ProfileEditPage from "../page/ProfileEditPage";
-
 import InfiniteScroll from "../components/main/InfiniteScroll";
 import PastEventsPage from "../page/PastEventsPage";
 import UserSearchPage from "../page/UserSearchPage";
@@ -26,38 +24,30 @@ import InputEmailPage from "../page/InputEmailPage";
 import FriendsMain from "../components/main/FriendsMain";
 import ChangePasswordPage from "../page/ChangePasswordPage";
 
-const Router = () => {
-  const nickname = sessionStorage.getItem("nickname");
 
+const Router = () => {
+  const userId = sessionStorage.getItem("userId");
+  console.log(userId)
   return (
-    <Container>
       <Layout>
         <Routes>
           {/* 메인과 디테일 페이지 */}
-          {/* <Route path="/main" element={<MainPage />} /> */}
-          <Route path="/main" element={<MainPage />} />
-          <Route path="/friends/:id" element={<FriendsMain />} />
           {/* <Route path="/main" element={<InfiniteScroll />} /> */}
-          {nickname ? (
-            <Route path="/" element={<MainPage />} />
-          ) : (
-            <Route path="/" element={<LoginPage />} />
-          )}
-          <Route path="/detail/:id" element={<ScheduleDetailPage />} />
-          <Route path="/friendsdetail/:id" element={<ScheduleDetailPage />} />
-          <Route path="/schedule" element={<ScheduleRegisterPage />} />
-          <Route path="/schedule/edit" element={<ScheduleRegisterPage />} />
+          <Route path="/" element={userId? <MainPage /> : <Navigate replace to='/login' />} />
+          <Route path="/main" element= {userId? <MainPage /> : <Navigate replace to='/login' />} />
+          <Route path="/detail/:id" element={userId? <ScheduleDetailPage /> : <Navigate replace to='/login' />} />
+          <Route path="/friends/:id" element={userId? <FriendsMain /> : <Navigate replace to='/login' />} />  
+          <Route path="/friendsdetail/:id" element={userId? <ScheduleDetailPage /> : <Navigate replace to='/login' />} />
+          <Route path="/schedule" element={userId? <ScheduleRegisterPage /> : <Navigate replace to='/login' />} />
+          <Route path="/schedule/edit" element={userId? <ScheduleRegisterPage /> : <Navigate replace to='/login' />} />
 
           {/* 무한스크롤 */}
-          <Route path="/mainInfinite" element={<InfiniteScroll />} />
+          <Route path="/mainInfinite" element={userId? <InfiniteScroll /> : <Navigate replace to='/login' />} />
 
           {/* 회원가입 */}
-          <Route path="/signup" element={<SignupPage />} />
-          <Route
-            path="/signup/setProfileName"
-            element={<SetProfileNamePage />}
-          />
-          <Route path="/signup/setProfileImg" element={<SetProfileImgPage />} />
+          <Route path="/signup" element={userId? <SignupPage /> : <Navigate replace to='/login' />} />
+          <Route path="/signup/setProfileName" element={userId? <SetProfileNamePage /> : <Navigate replace to='/login' />}/>
+          <Route path="/signup/setProfileImg" element={userId? <SetProfileImgPage /> : <Navigate replace to='/login' />} />
 
           {/* 로그인 */}
           <Route path="/login" element={<LoginPage />} />
@@ -68,28 +58,24 @@ const Router = () => {
           <Route path="/login/auth/InputEmail" element={<InputEmailPage />} />
 
           {/* 마이페이지 */}
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/scheduleinvitation" element={<ScheduleInvitation />} />
-          <Route path="/follow" element={<FollowPage />} />
-          <Route path="/pastEvents" element={<PastEventsPage />} />
-          <Route path="/editProfile" element={<ProfileEditPage />} />
-          <Route path="/ChangePassword" element={<ChangePasswordPage />} />
+          <Route path="/profile" element={userId? <Profile /> : <Navigate replace to='/login' />} />
+          <Route path="/scheduleinvitation" element={userId? <ScheduleInvitation /> : <Navigate replace to='/login' />} />
+          <Route path="/follow" element={userId? <FollowPage /> : <Navigate replace to='/login' />} />
+          <Route path="/pastEvents" element={userId? <PastEventsPage /> : <Navigate replace to='/login' />} />
+          <Route path="/editProfile" element={userId? <ProfileEditPage /> : <Navigate replace to='/login' />} />
+          <Route path="/ChangePassword" element={userId? <ChangePasswordPage /> : <Navigate replace to='/login' />} />
+
           {/* 알람 */}
-          <Route path="/notification" element={<NotificationsPage />} />
+          <Route path="/notification" element={userId? <NotificationsPage /> : <Navigate replace to='/login' />} />
 
           {/* 찾기 */}
-          <Route path="/userSearch" element={<UserSearchPage />} />
+          <Route path="/userSearch" element={userId? <UserSearchPage /> : <Navigate replace to='/login' />} />
+
         </Routes>
       </Layout>
-    </Container>
+   
   );
 };
 
 export default Router;
-const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  box-sizing: border-box;
-`;
+
