bfb15067d00b99579d6e825dde0c301089eb2491
Angelachaejung <a01083298599@gmail.com>
2023-03-11 19:09:44 +0900

    Design/Modal위치조정

--- src/components/modal/ScheduleAddModal.jsx
+++ src/components/modal/ScheduleAddModal.jsx
@@ -1,25 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ScheduleAddModal = ({ state }) => {
+  const navigate = useNavigate();
+  const closeModal = () => navigate("/main");
+
   return (
     <div>
       <div className="h-screen w-[375px] bg-black bg-opacity-50 flex justify-center items-center fixed z-10 mt-[-48px]">
-        <div className="pt-12 items-center w-[300px] h-[150px] text-center fixed top-[230px] rounded-[16px] bg-white">
+        <div className="pt-8  items-center w-[300px] h-[150px] text-center fixed top-[38%] rounded-[16px] bg-white">
           {state.type === "edit" ? (
-            <div className="text-[18px] font-bold flex flex-col">
-              일정이 수정 되었습니다!
-            </div>
+            <div className="text-[18px] font-bold flex flex-col">일정이 수정 되었습니다!</div>
           ) : (
-            <div className="text-[18px] font-bold flex flex-col">
-              일정이 추가되었습니다!
-            </div>
+            <div className="text-[18px] font-bold flex flex-col">일정이 추가되었습니다!</div>
           )}
-          {/* <button
-            className="bg-[#002C51] h-[40px] w-[268px] text-white mt-[18px]"
-            onClick={closeModal}
-          >
+          <button className="bg-[#002C51] mt-8 h-[40px] w-[268px] text-white  rounded-sm" onClick={closeModal}>
             확인
-          </button> */}
+          </button>
         </div>
       </div>
     </div>
