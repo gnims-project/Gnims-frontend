1e8faff0a1a37765a09cf3ea760b2e8fa237e292
Angelachaejung <a01083298599@gmail.com>
2023-03-11 19:07:43 +0900

    Feat/Notification-invite_Response 기능 추가구현

--- src/components/notification/NotificationsList.jsx
+++ src/components/notification/NotificationsList.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { EventSourcePolyfill } from "event-source-polyfill";
 import mentionIcon from "../../img/mention.svg";
 import followIcon from "../../img/follow.svg";
+import responseIcon from "../../img/response.svg";
+import rejectIcon from "../../img/reject.svg";
 import { useNavigate } from "react-router-dom";
 import { instance } from "../../shared/AxiosInstance";
 
@@ -10,19 +11,34 @@ const NotificationsList = () => {
   //notifications는 알림을 담는 배열이다.
   const [notifications, setNotifications] = useState([]);
   const [notification, setNotification] = useState([]);
-
+  const icon = (props) => {
+    if (props.notificationType === "FRIENDSHIP") {
+      return <img src={followIcon} alt="followIcon" className="h-[26px] w-[26px] flex " />;
+    } else if (props.notificationType === "SCHEDULE") {
+      return <img src={mentionIcon} alt="mentionIcon" className="h-[26px] w-[26px] flex " />;
+    } else if (props.notificationType === "INVITE_RESPONSE") {
+      if (props.message.includes("거절")) {
+        return <img src={rejectIcon} alt="rejectIcon" />;
+      } else return <img src={responseIcon} alt="responseIcon" className="h-[22px] w-[22px] flex" />;
+    }
+  };
+  const path = (props) => {
+    if (props === "FRIENDSHIP") {
+      return navigate("/follow");
+    } else if (props === "SCHEDULE") {
+      return navigate("/scheduleinvitation");
+    } else if (props === "INVITE_RESPONSE") {
+      return navigate("/main");
+    }
+  };
   const readAll = async () => {
     await instance.put("/notifications");
-    // const promises = notification.map((noti) =>
-    //   instance.get(`/notifications/${noti.id}`)
-    // );
-    // await Promise.all(promises);
-    // 여기서 responses 배열을 처리한다.
     window.location.reload();
   };
-
+  console.log(notification);
   const getNoti = async () => {
     await instance.get("/notifications").then((res) => {
+      console.log(res);
       const newNotifications = res.data.data.map((data) => ({
         id: data.notificationId,
         message: data.message,
@@ -43,67 +59,6 @@ const NotificationsList = () => {
     const newNotification = [...new Set(notifications.map(JSON.stringify))].map(JSON.parse);
     setNotification(newNotification);
   }, [notifications]);
-
-  useEffect(() => {
-    let eventSource;
-    const fetchSse = async () => {
-      try {
-        //EventSource생성.
-        eventSource = new EventSourcePolyfill("https://eb.jxxhxxx.shop/connection", {
-          //headers에 토큰을 꼭 담아줘야 401이 안뜬다.
-          headers: {
-            Authorization: sessionStorage.getItem("accessToken"),
-          },
-          withCredentials: true,
-        });
-        // SSE 연결 성공 시 호출되는 이벤트 핸들러
-        eventSource.onopen = () => {
-          // console.log("SSE onopen");
-        };
-        //알림이 왔을 때 취할 액션은 이 아래에.
-        eventSource.onmessage = async (event) => {
-          const data = await JSON.parse(event.data);
-        };
-        // 연결시에 콘솔이 찍힌다.
-        eventSource.addEventListener("connect", (event) => {
-          // console.log(event.data);
-        });
-
-        eventSource.addEventListener("invite", (event) => {
-          const data = JSON.parse(event.data);
-          // alert("초대가 도착했습니다", data.message);
-          const newNotification = {
-            id: data.notificationId,
-            message: data.message,
-            notificationType: data.notificationType,
-            date: data.dateTime.toString().split("T")[0],
-            isChecked: data.isChecked,
-          };
-          // alert("invite메세지 도착! parsing한거", data);
-          setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-          setNotification([...new Set(notifications.map(JSON.stringify))].map(JSON.parse));
-        });
-
-        eventSource.addEventListener("follow", (event) => {
-          const data = JSON.parse(event.data);
-          const newNotification = {
-            id: data.notificationId,
-            message: data.message,
-            notificationType: data.notificationType,
-            date: data.dateTime.toString().split("T")[0],
-            isChecked: data.isChecked,
-          };
-
-          setNotifications((prevNotifications) => [newNotification, ...prevNotifications]);
-          setNotification([...new Set(notifications.map(JSON.stringify))].map(JSON.parse));
-        });
-      } catch (error) {
-        // console.log("에러발생:", error);
-      }
-    };
-    // @0307 14:20 현재페이지에서 CORS에러생기고 4번씩 요청보내는 문제가 있음. 알림페이지에서는 SSE연결 하지않는걸로.
-    // fetchSse();
-  }, [notification]);
 
   return (
     <div className="bg-[#FFFFFF] w-[375px] h-full">
@@ -127,16 +82,12 @@ const NotificationsList = () => {
                 }
                 onClick={() => {
                   instance.get(`/notifications/${notification.id}`);
-                  notification.notificationType === "SCHEDULE" ? navigate("/scheduleinvitation") : navigate("/follow");
+                  path(notification.notificationType);
                 }}
                 className="pl-[20px] pr-[20px] pt-[20px] h-[86px]  text-left text-[#121213] border-solid border-[rgb(219,219,219)] border-b-[1px]"
               >
                 <div>
-                  {notification.notificationType === "FRIENDSHIP" ? (
-                    <img src={followIcon} alt="followIcon" className="h-[26px] w-[26px] flex " />
-                  ) : (
-                    <img src={mentionIcon} alt="mentionIcon" className="h-[26px] w-[26px] " />
-                  )}
+                  <div>{icon(notification)}</div>
                   <div className="mt-[-30px] text-[14px] ml-[50px]">{notification.message}</div>
                   <br />
                   <span className="text-[#6F6F6F] text-[13px] ml-[50px]">
