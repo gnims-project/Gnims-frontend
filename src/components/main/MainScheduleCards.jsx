8fb22969a2e99c18fbe59790f88bfdaae93b55a4
Angelachaejung <a01083298599@gmail.com>
2023-03-16 21:23:12 +0900

    Design/케밥버튼, 메인페이지 카드 애니메이션효과

--- src/components/main/MainScheduleCards.jsx
+++ src/components/main/MainScheduleCards.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const MainScheduleCards = ({ schedules }) => {
   const navigate = useNavigate();
@@ -28,9 +29,7 @@ const MainScheduleCards = ({ schedules }) => {
     if (invitees.length > 1) {
       setInviteesList(() => ({
         hidden: false,
-        inviteesList: `${schedules.invitees[0].username}외 ${
-          invitees.length - 1
-        }명`,
+        inviteesList: `${schedules.invitees[0].username}외 ${invitees.length - 1}명`,
       }));
     }
   }, [invitees, schedules.invitees]);
@@ -40,13 +39,18 @@ const MainScheduleCards = ({ schedules }) => {
   };
 
   return (
-    <div
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01],
+      }}
       onClick={onDetail}
       className={`w-[335px] h-[180px] bg-white rounded-[10px] border border-solid border-[#E8E8E8]  shadow-md  cursor-pointer`}
     >
-      <div
-        className={`flex items-center  h-[14px] ${bgColor} rounded-t-[10px] `}
-      >
+      <div className={`flex items-center  h-[14px] ${bgColor} rounded-t-[10px] `}>
         <ul className="ml-[9px] flex flex-row gap-[4px]">
           {[0, 1, 2].map((list) => (
             <li key={list} className="bg-white h-[4px] w-[4px] rounded-full" />
@@ -85,10 +89,7 @@ const MainScheduleCards = ({ schedules }) => {
                   <div className="flex -space-x-8 overflow-hidden ">
                     {invitees?.map((list, index) => {
                       return (
-                        <div
-                          key={index}
-                          className="flex border-2 border-white rounded-full"
-                        >
+                        <div key={index} className="flex border-2 border-white rounded-full">
                           <img
                             className="inline-block h-[40px] w-[40px] rounded-full"
                             src={list.profile}
@@ -98,16 +99,14 @@ const MainScheduleCards = ({ schedules }) => {
                       );
                     })}
                   </div>
-                  <div className="flex items-center text-[#6F6F6F]">
-                    {inviteesList.inviteesList}
-                  </div>
+                  <div className="flex items-center text-[#6F6F6F]">{inviteesList.inviteesList}</div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
